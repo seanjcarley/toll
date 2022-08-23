09e14b9216b815052289f7749c5c5a107fb77086
Sean Carley <seanjcarley@gmail.com>
2022-08-23 16:30:26 +0100

    added toasts

--- user_account/views.py
+++ user_account/views.py
@@ -9,12 +9,18 @@ from .forms import UserProfileForm
 # Create your views here.
 @login_required
 def show_user_profile(request):
-    ''' show user profile '''
-    profile = get_object_or_404(User, username=request.user)
-    print(profile.email)
+    ''' show user profile and allow to update selected details '''
+    profile1 = get_object_or_404(UserProfile, user=request.user)
+    profile2 = get_object_or_404(User, username=request.user)
+
+    fname = profile2.first_name
+    sname = profile2.last_name
+    uemail =profile2.email
+    uname = profile2.username
+    uphone = profile1.phone
 
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+        form = UserProfileForm(request.POST, instance=profile1)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully.')
@@ -23,12 +29,24 @@ def show_user_profile(request):
                 request, 'Update failed! Please enter all required details.'
             )
     else:
-        form = UserProfileForm(instance=profile)
+        form = UserProfileForm(instance=profile1)
 
     template = 'user_account/profile.html'
 
     context = {
         'form': form,
+        'uname': uname,
+        'fname': fname,
+        'sname': sname,
+        'uemail': uemail,
+        'uphone': uphone,
     }
 
     return render(request, template, context)
+
+
+def create_user_profile(request):
+    ''' create a new user '''
+    form = UserProfileForm()
+
+    return render(request, form)

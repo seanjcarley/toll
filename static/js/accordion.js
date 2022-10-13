9551a58045f21c4cece3cbfe708de0aae8287280
Sean Carley <seanjcarley@gmail.com>
2022-10-13 16:33:00 +0100

    added navbar to includesfor personal and vehicle pages, and made accordian changes

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -1,17 +1,54 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// the below functions act in the associated panel and ensure that 
+// the other panels contract
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* toggle the active class */
-        this.classList.toggle("active");
+document.getElementById("acc_login").addEventListener("click", expandLogin);
+document.getElementById("acc_vehicle").addEventListener("click", expandVehicle);
+document.getElementById("acc_contact").addEventListener("click", expandContact);
 
-        /* toggle panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+function expandLogin() {
+    this.classList.toggle("active");
+    panel1 = document.getElementById("login_panel");
+    panel2 = document.getElementById("vehicle_panel");
+    panel3 = document.getElementById("contact_panel");
+    if (panel1.style.maxHeight) {
+        panel1.style.maxHeight = null;
+        panel2.style.maxHeight = null;
+        panel3.style.maxHeight = null;
+    } else {
+        panel1.style.maxHeight = panel1.scrollHeight + "px";
+        panel2.style.maxHeight = null;
+        panel3.style.maxHeight = null;
+    }
+}
+
+function expandVehicle() {
+    this.classList.toggle("active");
+    panel1 = document.getElementById("login_panel");
+    panel2 = document.getElementById("vehicle_panel");
+    panel3 = document.getElementById("contact_panel");
+    if (panel2.style.maxHeight) {
+        panel1.style.maxHeight = null;
+        panel2.style.maxHeight = null;
+        panel3.style.maxHeight = null;
+    } else {
+        panel1.style.maxHeight = null;
+        panel2.style.maxHeight = panel2.scrollHeight + "px";
+        panel3.style.maxHeight = null;
+    }
+}
+
+function expandContact() {
+    this.classList.toggle("active");
+    panel1 = document.getElementById("login_panel");
+    panel2 = document.getElementById("vehicle_panel");
+    panel3 = document.getElementById("contact_panel");
+    if (panel3.style.maxHeight) {
+        panel1.style.maxHeight = null;
+        panel2.style.maxHeight = null;
+        panel3.style.maxHeight = null;
+    } else {
+        panel1.style.maxHeight = null;
+        panel2.style.maxHeight = null;
+        panel3.style.maxHeight = panel3.scrollHeight + "px";
+    }
 }
